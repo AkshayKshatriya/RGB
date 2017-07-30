40b5dd6c84e61c12f83855388795ae65046f131e
mutterflyios <akshay@Akshays-MacBook-Air.local>
2017-07-30 13:26:42 +0530

    Project and dependencies set up

---
+++ RGB/RGB.h
@@ -0,0 +1,19 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by Akshay Gawade on 30/07/17.
+//  Copyright © 2017 Akshay Gawade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
